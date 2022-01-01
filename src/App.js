760c84ba18067978d8d64496ed8b9fdf08929218
Neil <nkurien97@gmail.com>
2022-01-01 19:37:12 +0530

    removed unnecessary import statements

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import React from "react";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
 
-//Pages
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
-import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
 
-//Router
-import { Link } from "react-router-dom";
 import { About } from "./styles";
 
 function App() {
