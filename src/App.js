8398c1527cd1f8cbd7f323dfe56aba6bf51ebcf1
Neil <nkurien97@gmail.com>
2022-01-01 19:35:07 +0530

    added react-router6 with routes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import GlobalStyle from "./components/GlobalStyle";
 
 //Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
 
+//Router
+import { Link } from "react-router-dom";
+import { About } from "./styles";
+
 function App() {
-  return (
-    <div className="App">
-      <GlobalStyle />
-      <Nav />
-      <AboutUs />
-    </div>
-  );
+	return (
+		<div className="App">
+			<GlobalStyle />
+			<Nav />
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+//Pages
+import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />}>
+					<Route path="/" element={<AboutUs />}></Route>
+					<Route path="/work" element={<OurWork />}></Route>
+					<Route path="/contact" element={<ContactUs />}></Route>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
