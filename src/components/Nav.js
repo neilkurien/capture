02ea43af1cadfd453be3b40c372ba6598080e669
Neil <nkurien97@gmail.com>
2022-01-03 01:26:36 +0530

    added mobile layout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -54,6 +54,22 @@ const StyledNav = styled.nav`
 		padding-left: 10rem;
 		position: relative;
 	}
+	@media (max-width: 1300px) {
+		flex-direction: column;
+		padding: 2rem 1rem;
+		ul {
+			padding: 2rem;
+			justify-content: space-around;
+			width: 100%;
+		}
+		li {
+			padding-left: 0;
+		}
+		#logo {
+			display: inline-block;
+			margin: 1rem;
+		}
+	}
 `;
 
 export default Nav;
