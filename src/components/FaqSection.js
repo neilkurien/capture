4ffe53acf6680bfc4b4e081bdab9dfb8293590a9
Neil <nkurien97@gmail.com>
2022-01-02 19:32:52 +0530

    finished faq animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,82 +1,82 @@
 import React from "react";
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
-  return (
-    <Faq>
-      <h2>
-        Any Questions <span>FAQ</span>
-      </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus,
-            nisi.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus,
-            nisi.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus,
-            nisi.
-          </p>
-        </div>
-      </div>
-      <div className="question">
-        <h4>What Products do you Offer?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus,
-            nisi.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-    </Faq>
-  );
+	return (
+		<Faq>
+			<h2>
+				Any Questions <span>FAQ</span>
+			</h2>
+			<AnimateSharedLayout>
+				<Toggle title="How Do I Start?">
+					<div className="answer">
+						<p>Lorem ipsum dolor sit amet.</p>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing
+							elit. Possimus, nisi.
+						</p>
+					</div>
+				</Toggle>
+
+				<Toggle title="Daily Schedule">
+					<div className="answer">
+						<p>Lorem ipsum dolor sit amet.</p>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing
+							elit. Possimus, nisi.
+						</p>
+					</div>
+				</Toggle>
+
+				<Toggle title="Different Payment Methods">
+					<div className="answer">
+						<p>Lorem ipsum dolor sit amet.</p>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing
+							elit. Possimus, nisi.
+						</p>
+					</div>
+				</Toggle>
+
+				<Toggle title="What Products do you Offer?">
+					<div className="answer">
+						<p>Lorem ipsum dolor sit amet.</p>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing
+							elit. Possimus, nisi.
+						</p>
+					</div>
+				</Toggle>
+			</AnimateSharedLayout>
+		</Faq>
+	);
 };
 
 const Faq = styled(About)`
-  display: block;
-  span {
-    display: block;
-  }
-  h2 {
-    padding-bottom: 2rem;
-    font-weight: lighter;
-  }
-  .faq-line {
-    background: #cccccc;
-    height: 0.2rem;
-    margin: 2rem 0rem;
-    width: 100%;
-  }
-  .question {
-    padding: 3rem 0rem;
-    cursor: pointer;
-  }
-  p {
-    padding: 1rem 0rem;
-  }
+	display: block;
+	span {
+		display: block;
+	}
+	h2 {
+		padding-bottom: 2rem;
+		font-weight: lighter;
+	}
+	.faq-line {
+		background: #cccccc;
+		height: 0.2rem;
+		margin: 2rem 0rem;
+		width: 100%;
+	}
+	.question {
+		padding: 3rem 0rem;
+		cursor: pointer;
+	}
+	p {
+		padding: 1rem 0rem;
+	}
 `;
 
 export default FaqSection;
